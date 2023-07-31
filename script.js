5afa7ce1031ad77e073c3f73f64391743fbe056d
Satyam Goyal <satyamgoyal123456789@gmail.com>
2023-07-31 07:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// your code here
+function setUrl(){
+	const name=document.getElementById("name");
+	const year=document.getElementById("year");
+	let url="https://localhost:8080/?";
+	if(name.value===""){
+		url=url+"year="+year.value;
+	}else if(year.value===""){
+		url=url+"name="+name.value
+	}else{
+		url=url+"name="+name.value+"&year="+year.value
+	}
+	document.getElementById("url").innerHTML=url;
+}
